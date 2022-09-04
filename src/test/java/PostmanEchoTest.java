d5d767c6d32a3d8cc0ac8873e16ebc1985778853
swesda <swesda@yandex.ru>
2022-09-04 12:59:38 +0300

    Fourth commit

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("dat", equalTo("Postman"))
+                .body("data", equalTo("Postman"))
         ;
     }
 }
