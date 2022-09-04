af9144e0dc50bd3aa96eee41257db03dd800b097
swesda <swesda@yandex.ru>
2022-09-04 12:48:41 +0300

    Initial Commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void test() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Postman") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Postman"))
+        ;
+    }
+}
